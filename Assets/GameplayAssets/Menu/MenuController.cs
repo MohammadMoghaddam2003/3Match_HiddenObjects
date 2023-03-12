eead392144293d87dc2755a0fc5db55367204620
Mohammad Moqaddam <94843766+MohammadMoghaddam2003@users.noreply.github.com>
2023-03-12 18:08:24 +0330

    Fixed menu scene elements access to Audio Controller bug

--- Assets/GameplayAssets/Menu/MenuController.cs
+++ Assets/GameplayAssets/Menu/MenuController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Game_Manager;
+using GameplayAssets.Audio;
 using GameplayAssets.Level;
 
 namespace GameplayAssets.Menu
@@ -11,24 +12,37 @@ namespace GameplayAssets.Menu
 
 
         private LevelController _levelController;
+        private AudioController _audioController;
 
 
-        private void Awake() => DontDestroyOnLoad(this.gameObject);
 
-        private void Start() => _levelController = GameManager.Instance.GetLevelController;
+        private void Start()
+        {
+            _levelController = GameManager.Instance.GetLevelController;
+            _audioController = GameManager.Instance.GetAudioController;
+        } 
         
         public void GoStartMenu()
         {
+            PlayClickSound();
             startMenu.SetActive(true);
             signinMenu.SetActive(false);
         }
         
         public void GoSigninMenu()
         {
+            PlayClickSound();
             signinMenu.SetActive(true);
             startMenu.SetActive(false);
         }
 
-        public void SigninCompleted() => _levelController.LoadLevel();
+        public void SigninCompleted()
+        {
+            PlayClickSound();
+            _levelController.LoadLevel();
+        } 
+        
+        public void PlayClickSound() => _audioController.Click();
+        
     }
 }
